b26c566af9b25391081b45fe6d498709495be4e4
Saranraj <www.saranrajcoder@gmail.com>
2024-05-05 04:32:20 +0530

    Food Menu Image Rotate Added

--- client/src/Components/FoodMenu/FoodMenu.jsx
+++ client/src/Components/FoodMenu/FoodMenu.jsx
@@ -8,7 +8,7 @@ const FoodMenu = ({ category, setCategory }) => {
             <div className='mt-[3rem] justify-center flex gap-[3rem]'>
                 {menu.map((item, index) => (
                     <div onClick={() => setCategory(prev => prev === item.name ? "All" : item.name)} key={index}>
-                        <img className={`rounded-full h-[5rem] ${category === item.name ? 'active' : ''}`} src={item.image} alt="" />
+                        <img className={`rounded-full hover:rotate-45 h-[5rem] ${category === item.name ? 'active' : ''}`} src={item.image} alt="" />
                         <h1 className='mt-1 font-bold font-Arimo text-center'>{item.name}</h1>
                     </div>
                 ))}
