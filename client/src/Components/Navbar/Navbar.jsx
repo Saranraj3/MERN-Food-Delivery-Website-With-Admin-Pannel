30d8eae1981727d0f6959933c14d42a77af7a0c1
Saranraj <www.saranrajcoder@gmail.com>
2024-05-04 13:02:09 +0530

    Navbar Redesign

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import { CiSearch } from "react-icons/ci";
+import { SiIfood } from "react-icons/si";
+import { IoSearch } from "react-icons/io5";
 import { IoCartSharp } from "react-icons/io5";
 import { MdAccountCircle } from "react-icons/md";
 
 function Navbar() {
     return (
-        <div>
-            <h1 className='mt-3 ml-5 cursor-pointer text-4xl font-mono font-bold text-blue-500'>Foody</h1>
-            <ul className='mt-[-2rem] font-bold font-serif cursor-pointer flex justify-center gap-[5rem] '>
-                <li className=' hover:w-[5rem] hover:h-[2rem] hover:rounded-full hover:text-center hover:pt-1 hover:bg-blue-500 hover:text-white'>Home</li>
-                <li className=' hover:w-[5rem] hover:h-[2rem] hover:rounded-full hover:text-center hover:pt-1 hover:bg-blue-500 hover:text-white'>Menu</li>
-                <li className=' hover:w-[5rem] hover:h-[2rem] hover:rounded-full hover:text-center hover:pt-1 hover:bg-blue-500 hover:text-white'>Service</li>
-                <li className=' hover:w-[5rem] hover:h-[2rem] hover:rounded-full hover:text-center hover:pt-1 hover:bg-blue-500 hover:text-white'>About</li>
+        <div className='sticky top-0 z-50'>
+            <span className='flex mt-2'>
+            <SiIfood className='mt-1 ml-5 cursor-pointer text-blue-500 size-[1.5rem]'/>
+            <h1 className=' ml-2 cursor-pointer text-4xl font-sedan font-bold text-blue-500'>Foody</h1>
+            </span>
+            <ul className='mt-[-2rem] font-bold font-serif text-white cursor-pointer flex justify-center gap-[5rem] '>
+                <li className='hover:text-blue-500'>Home</li>
+                <li className='hover:text-blue-500'>Menu</li>
+                <li className='hover:text-blue-500'>Service</li>
+                <li className='hover:text-blue-500'>About</li>
             </ul>
-            <ul className='mt-[-1.5rem] mr-[2rem] flex justify-end gap-[3rem]'>
-                <CiSearch className='cursor-pointer hover:text-blue-500 size-[1.5rem]' />
+            <ul className='mt-[-1.5rem] mr-[2rem] text-white flex justify-end gap-[3rem]'>
+                <IoSearch className='cursor-pointer hover:text-blue-500 size-[1.5rem]' />
                 <IoCartSharp className='cursor-pointer hover:text-blue-500 size-[1.5rem]' />
                 <MdAccountCircle className='cursor-pointer hover:text-blue-500 size-[1.5rem]' />
             </ul>
