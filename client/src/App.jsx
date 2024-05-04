d95d7b71465484a9202e6625b5e17fe530a77987
Saranraj <www.saranrajcoder@gmail.com>
2024-05-04 08:35:11 +0530

    Page Router Setted Some Pages Like Home Cart Order

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+import Order from './Pages/Order/Order';
+import Cart from './Pages/Cart/Cart';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Order />} />
+      </Routes>
     </div>
   )
 }
