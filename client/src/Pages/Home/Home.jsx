eaded218839dabc6d0dbb8290db4fda91eedf71a
Saranraj <www.saranrajcoder@gmail.com>
2024-05-04 13:37:03 +0530

    Banner Component Deleted

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Banner from '../../Components/Banner/Banner'
 import FoodMenu from '../../Components/FoodMenu/FoodMenu';
 import FoodItems from '../../Components/FoodItems/FoodItems';
 
 function Home() {
     return (
         <div>
-            <Banner />
             <FoodMenu />
             <FoodItems/>
         </div>
