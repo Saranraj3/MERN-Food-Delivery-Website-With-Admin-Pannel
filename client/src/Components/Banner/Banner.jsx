06987917b607a0fe23df17a3ee78dc68168662b0
Saranraj <www.saranrajcoder@gmail.com>
2024-05-05 04:17:22 +0530

    Banner Component Recreated

---
+++ client/src/Components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import banner from '../../Assets/Banner/banner.jpg'
+function Banner() {
+  return (
+    <div>
+        <img className='w-screen' src={banner} alt="" />
+    </div>
+  )
+}
+
+export default Banner
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
 import FoodMenu from '../../Components/FoodMenu/FoodMenu';
 import FoodItems from '../../Components/FoodItems/FoodItems';
+import Banner from '../../Components/Banner/Banner';
 
 function Home() {
     const [category,setCategory] = useState('All');
     return (
         <div>
+            <Banner/>
             <FoodMenu category={category} setCategory={setCategory}  />
             <FoodItems category={category}/>
         </div>
