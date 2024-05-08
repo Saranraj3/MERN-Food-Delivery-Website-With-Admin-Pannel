26cf5bbb1efe4871486f504a71858122dbf10942
Saranraj <www.saranrajcoder@gmail.com>
2024-05-08 10:11:54 +0530

    Add Page Fully Responsived

--- admin/src/Pages/Add/Add.jsx
+++ admin/src/Pages/Add/Add.jsx
@@ -55,31 +55,31 @@ function Add() {
 
   return (
     <div>
-      <form onSubmit={onSubmitHandle} className='text-white mt-[-12rem]'>
+      <form onSubmit={onSubmitHandle} className='sm:mt-[-10rem] md:mt-[-12rem] text-white'>
         <div className='text-center'>
-          <h1 className='cursor-pointer font-bold text-2xl'>Upload Image</h1>
+          <h1 className='sm:ml-5 sm:text-lg md:text-2xl cursor-pointer font-bold '>Upload Image</h1>
         </div>
         <div>
           <div className='justify-center flex'>
-            <img className='mt-3 w-[10rem]' src={file ? file : upload} alt="" />
+            <img className='sm:mt-3 sm:ml-5 sm:w-[6rem] md:mt-3 md:w-[10rem]' src={file ? file : upload} alt="" />
           </div>
-          <div className='mt-[-2rem] flex justify-center'>
-            <input onChange={handlchange} className='mt-[3rem] ml-[5rem]' type="file" name="imagefile" />
+          <div className='sm:mt-[-1.6rem] md:mt-[-2rem] flex justify-center'>
+            <input onChange={handlchange} className='sm:mt-[2.5rem] sm:ml-[5rem] sm:text-xs md:mt-[3rem] ml-[5rem]' type="file" name="imagefile" />
           </div>
         </div>
-        <div className='justify-center flex'>
-          <label className='mt-5 text-lg font-bold' htmlFor="">Product Name</label>
+        <div className='sm:mt-3 sm:ml-[5rem] justify-center flex'>
+          <label className='sm:mt-1 sm:text-xs md:mt-5 md:text-lg font-bold' htmlFor="">Name</label>
           <div>
-            <input onChange={OnChangeHandle} value={data.name} className='mt-5 ml-3 pl-3 h-[2rem] w-[19rem] border' type="text" name='name' placeholder='Product Type' />
+            <input onChange={OnChangeHandle} value={data.name} className='sm:ml-2 sm:w-[8rem] sm:h-[1.5rem] sm:text-xs md:mt-5 md:ml-3 md:h-[2rem] md:w-[19rem] pl-3 border' type="text" name='name' placeholder='Product Type' />
           </div>
         </div>
-        <div className='ml-[-3rem] justify-center flex'>
-          <label className='mt-5 text-lg font-bold' htmlFor="">Product Description</label>
-          <textarea onChange={OnChangeHandle} value={data.description} className=' mt-5 ml-3 pt-3 pl-2 w-[19rem] border resize-none' name="description" rows={6} placeholder='Write Down' id=""></textarea>
+        <div className='sm:mt-4 sm:ml-[3rem] md:ml-[2rem] justify-center flex'>
+          <label className='sm:text-xs md:mt-5 md:text-lg font-bold' htmlFor="">Description</label>
+          <textarea onChange={OnChangeHandle} value={data.description} className='sm:ml-2 sm:pt-2 sm:w-[8rem] sm:h-[8rem] sm:text-xs md:mt-5 md:ml-3 md:w-[19rem] pt-3 pl-2 border resize-none' name="description" rows={6} placeholder='Write Down' id=""></textarea>
         </div>
-        <div className='mr-[14.5rem] justify-center flex'>
-          <label className='mt-5 text-lg font-bold' htmlFor="">Product Category</label>
-          <select onChange={OnChangeHandle} className='mt-5 ml-3 border' name="category" >
+        <div className='sm:mt-4 md:mr-[12rem] justify-center flex'>
+          <label className='sm:text-xs md:mt-5 md:text-lg font-bold' htmlFor="">Category</label>
+          <select onChange={OnChangeHandle} className='sm:ml-2 sm:text-xs sm:w-[4rem] md:mt-5 md:ml-3 border' name="category" >
             <option value="Meat">Meat</option>
             <option value="Biriyani">Biriyani</option>
             <option value="Fryed Rice">Fryed Rice</option>
@@ -93,12 +93,12 @@ function Add() {
             <option value="KFC">KFC</option>
           </select>
         </div>
-        <div className='mt-[-1.9rem] ml-[13.5rem] justify-center flex'>
-          <label className='text-lg font-bold' htmlFor="">Price</label>
-          <input onChange={OnChangeHandle} value={data.price} className='ml-2 pl-2 border h-[2rem] w-[6rem]' type="number" name='price' placeholder='Rs. 400' />
+        <div className='sm:mt-4 sm:ml-[1.5rem] md:mt-[-1.9rem] md:ml-[13.5rem] justify-center flex'>
+          <label className='sm:text-xs md:text-lg font-bold' htmlFor="">Price</label>
+          <input onChange={OnChangeHandle} value={data.price} className='sm:ml-2 sm:text-xs sm:w-[4rem] sm:h-[1rem] md:ml-2 pl-2 border md:h-[2rem] md:w-[6rem]' type="number" name='price' placeholder='Rs. 400' />
         </div>
-        <div className='ml-[5rem] justify-center flex'>
-          <button className='mt-5 hover:opacity-80 bg-blue-500 rounded-md h-[2rem] w-[8rem] font-bold text-lg'>Add</button>
+        <div className='md:ml-[3rem] justify-center flex'>
+          <button className='sm:mt-4 sm:ml-5 sm:text-xs sm:h-[1.5rem] sm:w-[5rem] md:mt-5 md:h-[2rem] md:w-[8rem] md:text-lg hover:opacity-80 bg-blue-500 rounded-md font-bold'>Add</button>
         </div>
       </form>
     </div>
