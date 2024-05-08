4e7c561d20c41c3ea69cdd0b0e1759977d7f748f
Saranraj <www.saranrajcoder@gmail.com>
2024-05-08 09:16:08 +0530

    Navbar Fully Responsived

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -7,13 +7,13 @@ function Navbar() {
     <div className='text-white'>
       <span className='flex'>
         <SiIfood className='mt-5 ml-5 size-[1.8rem] cursor-pointer text-blue-500' />
-        <h1 className='mt-4 ml-2 text-3xl cursor-pointer font-sedan font-bold text-blue-500'>Foody</h1>
+        <h1 className='sm:invisible md:visible md:text-3xl mt-4 ml-2 cursor-pointer font-sedan font-bold text-blue-500'>Foody</h1>
       </span>
-      <h1 className='mt-[-2rem] text-2xl cursor-pointer font-sedan font-bold text-center text-blue-500'>ADMIN PANEL</h1>
+      <h1 className='sm:mt-[-1.6rem] md:text-2xl md:mt-[-2rem] cursor-pointer font-sedan font-bold text-center text-blue-500'>ADMIN PANEL</h1>
       <div className='flex justify-end'>
-        <MdAccountCircle className='mt-[-2rem] mr-[1.5rem] size-[2.2rem] cursor-pointer hover:text-blue-500 ' />
+        <MdAccountCircle className='sm:mt-[-1.8rem] md:mt-[-2rem] mr-[1.5rem] size-[2.2rem] cursor-pointer hover:text-blue-500 ' />
       </div>
-      <hr className='mt-3' />
+      <hr className='sm:mt-2 md:mt-3' />
     </div>
   )
 }
