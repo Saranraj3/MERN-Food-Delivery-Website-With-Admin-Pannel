bdb8e1ef4a88ca99a3d9a1148e9db3c1dcf809f5
Saranraj <www.saranrajcoder@gmail.com>
2024-05-08 09:23:33 +0530

    Sidebar Fully Responsived

--- admin/src/Components/Sidebar/Sidebar.jsx
+++ admin/src/Components/Sidebar/Sidebar.jsx
@@ -7,19 +7,19 @@ import { FaBoxArchive } from "react-icons/fa6";
 function Sidebar() {
   return (
     <div className='text-white'>
-      <Link to='/add'><div className='mt-[3rem] ml-5 flex '>
+      <Link to='/add'><div className='sm:mt-[1rem] md:mt-[3rem] ml-5 flex '>
         <IoMdAddCircle className='size-[2rem]' />
-        <h1 className='mt-1 ml-2 text-xl font-bold hover:text-blue-500'>Add</h1>
+        <h1 className='sm:invisible md:visible mt-1 ml-2 text-xl font-bold hover:text-blue-500'>Add</h1>
       </div></Link>
       <Link to='/list'><div className='mt-[2rem] ml-5 flex'>
         <FaClipboardList className='size-[2rem]' />
-        <h1 className='mt-1 ml-2 text-xl font-bold hover:text-blue-500'>List</h1>
+        <h1 className='sm:invisible md:visible mt-1 ml-2 text-xl font-bold hover:text-blue-500'>List</h1>
       </div></Link>
       <Link to='/order'><div className='mt-[2rem] ml-5 flex'>
         <FaBoxArchive className='size-[2rem]' />
-        <h1 className='mt-1 ml-2 text-xl font-bold hover:text-blue-500'>Order</h1>
+        <h1 className='sm:invisible md:visible mt-1 ml-2 text-xl font-bold hover:text-blue-500'>Order</h1>
       </div></Link>
-      <p className="mt-[-13rem] ml-[14rem] fixed inline-block h-[30.3rem] min-h-[1em] w-0.5 self-stretch bg-neutral-100 dark:bg-white"></p>
+      <p className="sm:mt-[-10.9rem] sm:ml-[4rem] md:mt-[-13rem] md:ml-[10rem] fixed inline-block h-[30.3rem] min-h-[1em] w-0.5 self-stretch bg-neutral-100 dark:bg-white"></p>
     </div>
   )
 }
